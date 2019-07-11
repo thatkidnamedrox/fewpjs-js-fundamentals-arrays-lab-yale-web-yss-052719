2b0c8e976b9c41d10a43b588a425b3040e3792d7
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:00:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Write your solution here!
 
-let pets = ["Milo", "Otis", "Garfield"]
+const append = ["Milo", "Otis", "Garfield"]
+const prepend = ["Milo", "Otis", "Garfield"]
+const removeLast = ["Milo", "Otis", "Garfield"]
+const removeFirst = ["Milo", "Otis", "Garfield"]
 
 pets.append("Odie");
