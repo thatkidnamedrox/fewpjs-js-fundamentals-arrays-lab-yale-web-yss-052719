f1b9314a42f1bbdee0619652b39f6f57754dfc53
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const append = ['Milo', 'Otis', 'Garfield'];
 const prepend = ['Milo', 'Otis', 'Garfield'];
 const removeLast = ['Milo', 'Otis', 'Garfield'];
-// const removeFirst = ['Milo', 'Otis', 'Garfield'];
+const removeFirst = ['Milo', 'Otis', 'Garfield'];
 
 append.push("Odie");
 prepend.unshift('Odie');
