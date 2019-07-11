ce140443a9782e6e5c2c98641b5852c5b5374257
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:03:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your solution here!
 
-const append = ['Milo', 'Otis', 'Garfield']
-const prepend = ['Milo', 'Otis', 'Garfield']
-const removeLast = ['Milo', 'Otis', 'Garfield']
-const removeFirst = ['Milo', 'Otis', 'Garfield']
+const append = ['Milo', 'Otis', 'Garfield'];
+const prepend = ['Milo', 'Otis', 'Garfield'];
+const removeLast = ['Milo', 'Otis', 'Garfield'];
+const removeFirst = ['Milo', 'Otis', 'Garfield'];
 
 append.append('Odie');
 prepend.prepend('Odie');
